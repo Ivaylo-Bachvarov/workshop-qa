3164129cc1d08e2231b039a8f1c5b428e3088dab
Ivaylo Bachvarov <ivaylo@hacksoft.io>
2024-10-30 08:17:15 +0200

    Progress from live coding

--- pages.py
+++ pages.py
@@ -1,18 +1,28 @@
-from playwright.sync_api import Page
+from constants import BASE_URL
 
 
 class LoginPage:
-    def __init__(self) -> None:
-        self.url = 'https://automation-workshop.hacksoft.io/admin/'
+    def __init__(self, page):
+        self.page = page
+        self.url = BASE_URL + '/admin/login/'
 
-    def goto(self, page):
-        page.goto(self.url)
+    def goto(self):
+        self.page.goto(self.url)
 
-    def username_field(self, page: Page):
-        return page.locator('#id_username')
+    def username_field(self):
+        return self.page.locator('#id_username')
 
-    def password_field(self, page: Page):
-        return page.locator('#id_password')
+    def password_field(self):
+        return self.page.locator('#id_password')
 
-    def submit_button(self, page: Page):
-        return page.locator('//*[@id="login-form"]/div[3]/input')
+    def submit_button(self):
+        return self.page.locator('//*[@id="login-form"]/div[3]/input')
+
+
+class AdminPage:
+    def __init__(self, page):
+        self.page = page
+        self.url = BASE_URL + '/admin/'
+
+    def goto(self):
+        self.page.goto(self.url)
--- test_auth.py
+++ test_auth.py
@@ -1,20 +1,29 @@
-from playwright.sync_api import Page
-from pages import LoginPage
-
+from playwright.sync_api import expect
+from pages import AdminPage, LoginPage
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def login_flow(page, username, password):
-    login_page = LoginPage()
-    login_page.goto(page)
-    login_page.username_field(page).fill(username)
-    login_page.password_field(page).fill(password)
-    login_page.submit_button(page).click()
+    login_page = LoginPage(page)
+    login_page.goto()
+    login_page.username_field().fill(username)
+    login_page.password_field().fill(password)
+    login_page.submit_button().click()
 
     return page
 
 
-def test_login_with_proper_credentials(page: Page):
-    page = login_flow(page, os.environ['USERNAME'], os.environ['PASSWORD'])
-    page.goto('https://automation-workshop.hacksoft.io/admin/reservations/property/')
-    print(page.locator('.field-address').count())
+def test_login_with_proper_credentials(page):
+    login_flow(page, os.getenv('USERNAME'), os.getenv('PASSWORD'))
+
+    admin_page = AdminPage(page)
+    expect(page).to_have_url(admin_page.url)
+
+
+def test_login_with_wrong_credentials(page):
+    login_flow(page, 'wrong_username', 'wrong_password')
+
+    error_message_element = page.locator('.errornote')
+    expect(error_message_element).to_be_visible()
